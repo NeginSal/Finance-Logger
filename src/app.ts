1af5a504a3ac9fc9a5279441c0abe04b01bc278d
NeginSal <neginsaljooghi@gmail.com>
2022-07-08 17:09:15 +0430

    DOM interaction and Typecasting

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+console.log(form.children);
+
+// inputs
+const type = document.querySelector('#type') as HTMLInputElement;
+const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
+
+form.addEventListener('submit', (e: Event) => {
+  e.preventDefault();
+
+  console.log(
+    type.value, 
+    tofrom.value, 
+    details.value, 
+    amount.valueAsNumber
+  );
+});
